d5751acd7b90dc968f1ca5b18cd73b815f7baeb8
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-03-13 16:43:49 +0700

    Ui in item description

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterwithfirebase/consts/consts.dart';
+import 'package:flutterwithfirebase/widget_common/our_button.dart';
 
 import '../../consts/colors.dart';
 
@@ -11,7 +12,7 @@ class ItemDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: lightGrey,
+      backgroundColor: whiteColor,
       appBar: AppBar(
         title: title!.text.color(darkFontGrey).fontFamily(bold).make(),
         actions: [
@@ -27,7 +28,155 @@ class ItemDetails extends StatelessWidget {
               ))
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  VxSwiper.builder(
+                      autoPlay: true,
+                      height: 350,
+                      itemCount: 3,
+                      aspectRatio: 16 / 9,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          imgFc5,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        );
+                      }),
+                  10.heightBox,
+                  title!.text
+                      .size(16)
+                      .color(darkFontGrey)
+                      .fontFamily(semibold)
+                      .make(),
+                  10.heightBox,
+                  VxRating(
+                    onRatingUpdate: (value) {},
+                    normalColor: textfieldGrey,
+                    selectionColor: golden,
+                    count: 5,
+                    size: 25,
+                    stepInt: true,
+                  ),
+                  10.heightBox,
+                  "\&30,000"
+                      .text
+                      .color(redColor)
+                      .fontFamily(bold)
+                      .size(18)
+                      .make(),
+                  10.heightBox,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            "Seller".text.white.fontFamily(semibold).make(),
+                            5.heightBox,
+                            "In House Brands"
+                                .text
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .size(16)
+                                .make(),
+                          ],
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.message_rounded,
+                          color: darkFontGrey,
+                        ),
+                      ),
+                    ],
+                  )
+                      .box
+                      .height(60)
+                      .padding(const EdgeInsets.symmetric(horizontal: 16))
+                      .color(textfieldGrey)
+                      .make(),
+                  20.heightBox,
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            child: "Color: ".text.color(textfieldGrey).make(),
+                          ),
+                          Row(
+                            children: List.generate(
+                                3,
+                                (index) =>
+                                    VxBox().size(40, 40).color(Vx.randomPrimaryColor).roundedFull.margin(EdgeInsets.symmetric(horizontal: 4)).make()),
+                          )
+                        ],
+                      ).box.padding(const EdgeInsets.all(8)).make(),
+
+
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            child: "Quantity: ".text.color(textfieldGrey).make(),
+                          ),
+                          Row(
+                            children: [
+                              IconButton(onPressed: (){}, icon: Icon(Icons.remove)),
+                              "0".text.size(16).color(darkFontGrey).fontFamily(bold).make(),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+                              10.widthBox,
+                              "(0 available)".text.color(textfieldGrey).make(),
+                            ],
+                          )
+                        ],
+                      ).box.padding(const EdgeInsets.all(8)).make(),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            child: "Total: ".text.color(textfieldGrey).make(),
+                          ),
+                          "\$0.00".text.color(redColor).size(16).fontFamily(bold).make(),
+                        ],
+                      ).box.padding(const EdgeInsets.all(8)).make(),
+                    ],
+                  ).box.white.shadowSm.make(),
+                  10.heightBox,
+                  "Description".text.color(darkFontGrey).fontFamily(semibold).make(),
+                  10.heightBox,
+                  "This is a dummy item and dummy description here... This is the space for description if we have data about the item in the database".text.color(darkFontGrey).make(),
+                  ListView(
+                    shrinkWrap: true,
+                    children: List.generate(5, (index)=>ListTile(
+                      title: "Video".text.fontFamily(semibold).color(darkFontGrey).make(),
+                      trailing: const Icon(Icons.arrow_forward),
+                    )),
+                  )
+                ],
+              ),
+            ),
+          )),
+          SizedBox(
+            width: double.infinity,
+            height: 60,
+            child: ourButton(
+                color: redColor,
+                onPress: () {},
+                textColor: whiteColor,
+                title: "Add to"),
+          )
+        ],
+      ),
     );
   }
 }
