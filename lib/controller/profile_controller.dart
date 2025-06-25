2e246d93e4216dc241b563a3bfae3a72a683bc68
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-06-25 16:41:57 +0700

    profile controller

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,11 +1,23 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutterwithfirebase/consts/consts.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:path/path.dart';
 
 class ProfileController extends GetxController {
   var profileImagePath = ''.obs;
+  var profileImageLink = '';
+
+  var isLoading = false.obs;
+
+  var nameController = TextEditingController();
+  var passController = TextEditingController();
 
   changeImage(context) async {
     var status = await Permission.photos.request();
@@ -15,12 +27,30 @@ class ProfileController extends GetxController {
     }
 
     try {
-      final img = await ImagePicker().pickImage(source: ImageSource.gallery, imageQuality: 70);
+      final img = await ImagePicker()
+          .pickImage(source: ImageSource.gallery, imageQuality: 70);
       if (img == null) return;
       profileImagePath.value = img.path;
     } on PlatformException catch (e) {
       VxToast.show(context, msg: e.toString());
     }
   }
-}
 
+  uploadProfileImage() async {
+    var fileName = basename(profileImagePath.value);
+    var destination = 'images/${currentUser!.uid}/$fileName';
+    Reference ref = FirebaseStorage.instance.ref().child(destination);
+    await ref.putFile(File(profileImagePath.value));
+    profileImageLink = await ref.getDownloadURL();
+  }
+
+  updateProfile({name, password, imageUrl}) async {
+    var store = firestore.collection(usersCollection).doc(currentUser!.uid);
+    await store.set({
+      'name': name,
+      'password': password,
+      'imageUrl': imageUrl,
+    }, SetOptions(merge: true));
+    isLoading(false);
+  }
+}
