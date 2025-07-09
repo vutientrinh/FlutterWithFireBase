53b7640eccdc914fbe1978c55890dc807babd4df
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-09 23:08:34 +0700

    firebase service

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -4,4 +4,7 @@ class FireStoreServices{
   static getUser(uid){
     return firestore.collection(usersCollection).doc(uid).snapshots();
   }
+  static getProducts(category){
+    return firestore.collection(productsCollection).where('p_category',isEqualTo: category).snapshots();
+  }
 }
