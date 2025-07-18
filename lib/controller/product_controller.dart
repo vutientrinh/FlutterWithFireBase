f981a5c01cd86491f9b1943417ede5a2821bda6e
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-18 17:31:00 +0700

    controller product

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -6,16 +6,36 @@ class ProductController extends GetxController {
   var quantity = 0.obs;
   var colorIndex = 0.obs;
   var subcat = [];
+  var totalPrice = 0.obs;
+
   getSubCategories(title) async {
     subcat.clear();
     var data = await rootBundle.loadString("lib/services/category_model.json");
     var decoded = categoryModelFromJson(data);
-    var s = decoded.categories.where((element)=> element.name == title).toList();
-    for (var e in s[0].subcategory){
+    var s =
+        decoded.categories.where((element) => element.name == title).toList();
+    for (var e in s[0].subcategory) {
       subcat.add(e);
     }
   }
-  changeColorIndex(index){
+
+  changeColorIndex(index) {
     colorIndex.value = index;
+  }
+
+  increaseQuantity(totalQuantity) {
+    if (quantity.value < totalQuantity) {
+      quantity.value++;
+    }
+  }
+
+  decreaseQuantity() {
+    if (quantity > 0) {
+      quantity.value--;
+    }
+  }
+
+  calculateTotalPrice(price) {
+    totalPrice = price * quantity.value;
   }
 }
