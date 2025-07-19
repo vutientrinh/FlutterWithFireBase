2356f9f024a9aff252d36ff33817aa734dc9207a
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-19 22:41:56 +0700

    exit dialog

---
+++ lib/widget_common/exit_dialog.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutterwithfirebase/consts/consts.dart';
+
+Widget exitDialog() {
+  return Dialog(child: Column().box.color(lightGrey).make());
+}
