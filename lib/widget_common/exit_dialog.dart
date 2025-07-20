6add92825e02ed2373293ead034c3ee5f87ce0ca
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-20 22:28:33 +0700

    exit dialog

--- lib/widget_common/exit_dialog.dart
+++ lib/widget_common/exit_dialog.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutterwithfirebase/consts/consts.dart';
+import 'package:flutterwithfirebase/widget_common/our_button.dart';
 
 Widget exitDialog() {
-  return Dialog(child: Column().box.color(lightGrey).make());
+  return Dialog(
+      child: Column(
+    children: [
+      "Confirm".text.fontFamily(bold).size(18).color(darkFontGrey).make(),
+      Divider(),
+      10.heightBox,
+      "Are you sure you want to exit?".text.size(16).color(darkFontGrey).make(),
+      10.heightBox,
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ourButton(
+              color: redColor,
+              onPress: () {},
+              textColor: whiteColor,
+              title: "Yes"),
+          ourButton(
+              color: redColor,
+              onPress: () {},
+              textColor: whiteColor,
+              title: "No")
+        ],
+      )
+    ],
+  ).box.color(lightGrey).roundedSM.make());
 }
