2979eafbfb3dd23941897cbccfa5aaa7a150acf9
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-07-23 17:41:48 +0700

    item detail

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -285,7 +285,18 @@ class ItemDetails extends StatelessWidget {
             height: 60,
             child: ourButton(
                 color: redColor,
-                onPress: () {},
+                onPress: () {
+                  controller.addToCart(
+                    color: data['p_colors'][controller.colorIndex.value],
+                    context: context,
+                    img: data['p_imgs'][0],
+                    qty: controller.quantity.value,
+                    sellerName: data['p_seller'],
+                    title: data['p_name'],
+                    tprice: controller.totalPrice.value,
+                  );
+                  VxToast.show(context, msg: "Added to cart");
+                },
                 textColor: whiteColor,
                 title: "Add to"),
           )
